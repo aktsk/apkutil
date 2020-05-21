bcc204cf9c43154b47fd0dc189012cef8ad0b68f
taichi-kotake <taichi.kotake@aktsk.jp>
2020-05-21 16:41:31 +0900

    add screenshot cmd, info cmd

--- apkpatch/apkutil.py
+++ apkpatch/apkutil.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 # coding: UTF-8
 
+import datetime
 import glob
 import json
 import os
 import subprocess
 
 
-def decode(apk_name):
+def decode(apk_path):
     apktool_cmd = ['apktool']
-    apktool_cmd.extend(['d', apk_name])
+    apktool_cmd.extend(['d', apk_path])
     proc = subprocess.Popen(apktool_cmd, stdout=subprocess.PIPE)
     outs, errs = proc.communicate()
     print(outs.decode('ascii'))
 
-def build(dir_name, apk_name):
+def build(dir_name, apk_path):
     apktool_cmd = ['apktool']
     apktool_cmd.extend(['b', dir_name])
-    apktool_cmd.extend(['-o', apk_name])
+    apktool_cmd.extend(['-o', apk_path])
     proc = subprocess.Popen(apktool_cmd, stdout=subprocess.PIPE)
     outs, errs = proc.communicate()
     print(outs.decode('ascii'))
 
-def sign(apk_name):
+def sign(apk_path):
     home_dir = os.environ['HOME']
     with open(home_dir + "/apkpatch.json") as f:
         config = json.load(f)
@@ -38,8 +39,48 @@ def sign(apk_name):
     apksigner_cmd.append('-v')
     apksigner_cmd.extend(['--ks-key-alias', ks_key_alias])
     apksigner_cmd.extend(['--ks-pass', ks_pass])
-    apksigner_cmd.append(apk_name)
+    apksigner_cmd.append(apk_path)
     proc = subprocess.Popen(apksigner_cmd, stdout=subprocess.PIPE)
     outs, errs = proc.communicate()
     print(outs.decode('ascii'))
 
+def get_packagename(apk_path):
+    home_dir = os.environ['HOME']
+    aapt_path = glob.glob(home_dir + '/Library/Android/sdk/build-tools/*/aapt')[0]
+    aapt_cmd = [aapt_path]
+    aapt_cmd.append('l')
+    aapt_cmd.append('-a')
+    aapt_cmd.append(apk_path)
+    aapt_proc = subprocess.Popen(aapt_cmd, stdout=subprocess.PIPE)
+    grep_proc = subprocess.Popen(["grep", "A: package"], stdin=aapt_proc.stdout, stdout=subprocess.PIPE)
+    aapt_proc.stdout.close()
+    outs, errs = grep_proc.communicate()
+    print(outs.decode('ascii'))
+
+def get_screenshot():
+    home_dir = os.environ['HOME']
+    adb_path = glob.glob(home_dir + '/Library/Android/sdk/platform-tools/adb')[0]
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y-%m-%d-%H-%M-%S")
+    screenshot_path = '/data/local/tmp/screenshot-' + timestamp + '.png'
+
+    screencap_cmd = [adb_path]
+    screencap_cmd.append('shell')
+    screencap_cmd.append('screencap')
+    screencap_cmd.extend(['-p', screenshot_path])
+    screencap_proc = subprocess.Popen(screencap_cmd, stdout=subprocess.PIPE)
+    outs, errs = screencap_proc.communicate()
+
+    pull_cmd = [adb_path]
+    pull_cmd.append('pull')
+    pull_cmd.append(screenshot_path)
+    pull_proc = subprocess.Popen(pull_cmd, stdout=subprocess.PIPE)
+    outs, errs = pull_proc.communicate()
+    print(outs.decode('ascii'))
+
+    rm_cmd = [adb_path]
+    rm_cmd.append('shell')
+    rm_cmd.append('rm')
+    rm_cmd.append(screenshot_path)
+    rm_proc = subprocess.Popen(rm_cmd, stdout=subprocess.PIPE)
+    outs, errs = rm_proc.communicate()
--- apkpatch/cli.py
+++ apkpatch/cli.py
@@ -11,10 +11,10 @@ from . import apkutil
 
 def cmd_todebuggable(args):
     print('Decoding APK by Apktool...')
-    apkutil.decode(args.apk_name)
+    apkutil.decode(args.apk_path)
 
     print('Checking AndroidManifest.xml...')
-    manifest = manifestutil.ManifestUtil(args.apk_name.replace('.apk', '') + '/AndroidManifest.xml')
+    manifest = manifestutil.ManifestUtil(args.apk_path.replace('.apk', '') + '/AndroidManifest.xml')
     manifest.check_all()
 
     colorama.init(autoreset=True)
@@ -22,44 +22,52 @@ def cmd_todebuggable(args):
     manifest.to_debuggable()
 
     print('\nBuilding APK by Apktool...')
-    dir_name = args.apk_name.replace('.apk', '')
-    apk_name = args.output
+    dir_name = args.apk_path.replace('.apk', '')
+    apk_path = args.output
     if args.output is None:
-        apk_name = dir_name + ".patched.apk"
-    apkutil.build(dir_name, apk_name)
-    print(Fore.CYAN + 'Output: ' + apk_name)
+        apk_path = dir_name + ".patched.apk"
+    apkutil.build(dir_name, apk_path)
+    print(Fore.CYAN + 'Output: ' + apk_path)
 
 def cmd_decode(args):
     print('Decoding APK by Apktool...')
-    apkutil.decode(args.apk_name)
+    apkutil.decode(args.apk_path)
 
     print('Checking AndroidManifest.xml...')
-    manifest = manifestutil.ManifestUtil(args.apk_name.replace('.apk', '') + '/AndroidManifest.xml')
+    manifest = manifestutil.ManifestUtil(args.apk_path.replace('.apk', '') + '/AndroidManifest.xml')
     manifest.check_all()
 
 def cmd_build(args):
     print('Building APK by Apktool...')
-    apk_name = args.output
+    apk_path = args.output
     if args.output is None:
-        apk_name = args.dir_name + ".patched.apk"
-    apkutil.build(args.dir_name, apk_name)
-    print(Fore.CYAN + 'Output: ' + apk_name)
+        apk_path = args.dir_name + ".patched.apk"
+    apkutil.build(args.dir_name, apk_path)
+    print(Fore.CYAN + 'Output: ' + apk_path)
 
 def cmd_sign(args):
     print('Signing APK by apksigner...')
-    apkutil.sign(args.apk_name)
+    apkutil.sign(args.apk_path)
+
+def cmd_info(args):
+    print('Getting package name by aapt...')
+    apkutil.get_packagename(args.apk_path)
+
+def cmd_screenshot(args):
+    print('Getting a screenshot from connected device...')
+    apkutil.get_screenshot()
 
 def main():
     parser = argparse.ArgumentParser(description='apk patcher')
     subparsers = parser.add_subparsers()
 
     parser_todebuggable = subparsers.add_parser('debuggable', help='')
-    parser_todebuggable.add_argument('apk_name', help='')
+    parser_todebuggable.add_argument('apk_path', help='')
     parser_todebuggable.add_argument('--output', '-o')
     parser_todebuggable.set_defaults(handler=cmd_todebuggable)
 
     parser_decompile = subparsers.add_parser('decode', aliases=['d'], help='')
-    parser_decompile.add_argument('apk_name', help='')
+    parser_decompile.add_argument('apk_path', help='')
     parser_decompile.set_defaults(handler=cmd_decode)
 
     parser_build = subparsers.add_parser('build', aliases=['b'], help='')
@@ -68,8 +76,15 @@ def main():
     parser_build.set_defaults(handler=cmd_build)
 
     parser_sign = subparsers.add_parser('sign', help='')
-    parser_sign.add_argument('apk_name', help='')
+    parser_sign.add_argument('apk_path', help='')
     parser_sign.set_defaults(handler=cmd_sign)
+
+    parser_info = subparsers.add_parser('info', help='')
+    parser_info.add_argument('apk_path', help='')
+    parser_info.set_defaults(handler=cmd_info)
+
+    parser_screenshot = subparsers.add_parser('screenshot', help='')
+    parser_screenshot.set_defaults(handler=cmd_screenshot)
 
     args = parser.parse_args()
     if hasattr(args, 'handler'):
