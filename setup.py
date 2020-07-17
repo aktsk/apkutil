4c6bae06b3aefab8e69cba17a2ac7f4be87cd5de
taichi-kotake <taichi.kotake@aktsk.jp>
2020-07-17 23:06:42 +0900

    fix err handling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="apkutil",
-    version="0.1.1",
+    version="0.1.2",
     description="decode, patch, build, etc",
     author="Taichi Kotake",
     packages=['apkutil'],
